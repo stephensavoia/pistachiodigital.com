8797fe1445a9e0baf6edf33a0095eb6785d01716
stephensavoia <ssavoia@gmail.com>
2024-05-08 13:04:35 -0400

    init

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('#menuToggle input[type="checkbox"]').addEventListener('change', function() {
+        let header = document.querySelector('header');
+        if (this.checked) {
+            header.classList.add('sticky');
+        } else {
+            header.classList.remove('sticky');
+        }
+    });
+
+    document.addEventListener('click', function(event) {
+        let menu = document.querySelector('#menu');
+        let menuToggle = document.querySelector('#menuToggle input[type="checkbox"]');
+        if (menuToggle.checked && !menu.contains(event.target) && event.target !== menuToggle) {
+            menuToggle.checked = false;
+        }
+    });
+    
+});
+
+let prevScrollpos = window.scrollY;
+window.onscroll = function() {
+    let currentScrollPos = window.scrollY;
+      if (prevScrollpos > currentScrollPos || currentScrollPos <= 0) {
+        document.querySelector("header").style.top = "0";
+      } else {
+        //if menuToggle isn't checked
+        if (!document.querySelector('#menuToggle input[type="checkbox"]').checked) {
+            document.querySelector("header").style.top = "-81px";
+        }
+      }
+      prevScrollpos = currentScrollPos;
+}
